b4fc67e1a303c2ddf9c5fe040cc68c81abdb362b
juancathree <juancarloslucenamonje@gmail.com>
2021-08-22 16:04:19 +0100

    feat: :sparkles: add translation component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+import detector from "i18next-browser-languagedetector";
 import "./normalize.css";
 import "./colors.css";
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+
+const options = {
+  order: ["navigator"],
+};
+
+i18next.use(detector).init({
+  detection: options,
+  interpolation: { escapeValue: false },
+  fallbackLng: "en",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
