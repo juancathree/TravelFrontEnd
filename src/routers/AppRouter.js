1021cdd75611beb87aafcb6817c434a0db676478
juancathree <juancarloslucenamonje@gmail.com>
2021-08-23 12:54:59 +0100

    refactor: :recycle: refactor router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,9 +2,9 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 
-const Home = lazy(() => import("../components/Home/index.js"));
 const Login = lazy(() => import("../components/Login/index.js"));
 const Signup = lazy(() => import("../components/Signup/index.js"));
+const Home = lazy(() => import("../components/Home/index.js"));
 
 export default function AppRouter() {
   return (
@@ -12,8 +12,10 @@ export default function AppRouter() {
       <BrowserRouter>
         <Switch>
           <PrivateRoute path="/" component={Home} exact />
-          <Route path="/login" render={() => <Login />} exact />
-          <Route path="/signup" render={() => <Signup />} exact />
+          <PrivateRoute path="/travels" component={Home} exact />
+          <PrivateRoute path="/user" component={Home} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/signup" component={Signup} exact />
           <Route path="*" />
         </Switch>
       </BrowserRouter>
