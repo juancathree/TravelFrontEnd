6918edf79fad460d221fded4c4df6523073c86ff
juancathree <juancarloslucenamonje@gmail.com>
2021-08-23 12:55:30 +0100

    feat: :sparkles: add cookies

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,11 @@
 import { useState, createContext } from "react";
+import Cookies from "universal-cookie";
 
 export const Context = createContext();
 
 export default function AuthContext({ children }) {
-  const [jwt, setJWT] = useState(() => window.localStorage.getItem("jwt"));
+  const cookie = new Cookies();
+  const [jwt, setJWT] = useState(() => cookie.get("travelapp"));
 
   return (
     <Context.Provider value={{ jwt, setJWT }}>{children}</Context.Provider>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import { useContext, useCallback, useState } from "react";
 import { Context } from "../context/AuthContext";
 import loginService from "../services/login";
+import Cookies from "universal-cookie";
 
 export default function useAuth() {
   const { jwt, setJWT } = useContext(Context);
@@ -9,14 +10,22 @@ export default function useAuth() {
   const login = useCallback(
     ({ email, password }) => {
       setState({ loading: true, error: false });
+      const cookies = new Cookies();
       loginService({ email, password })
         .then((jwt) => {
-          window.localStorage.setItem("jwt", jwt);
+          let d = new Date();
+          d.setTime(d.getTime() + 120 * 24 * 60 * 60 * 1000);
+          cookies.set("travelapp", jwt, {
+            path: "/",
+            sameSite: "none",
+            secure: true,
+            expires: d,
+          });
           setState({ loading: false, error: false });
           setJWT(jwt);
         })
         .catch((err) => {
-          window.localStorage.removeItem("jwt");
+          cookies.remove("travelapp");
           setState({ loading: false, error: true });
         });
     },
@@ -24,7 +33,8 @@ export default function useAuth() {
   );
 
   const logout = useCallback(() => {
-    window.localStorage.removeItem("jwt");
+    const cookies = new Cookies();
+    cookies.remove("travelapp");
     setJWT(null);
   }, [setJWT]);
 
