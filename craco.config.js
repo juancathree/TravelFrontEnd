f3ef843695d814a469d6b70e6c23086ef4f0fa19
juancathree <juancarloslucenamonje@gmail.com>
2021-08-26 17:03:25 +0100

    feat: :sparkles: add craco config

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const { whenProd } = require('@craco/craco')
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin')
+
+module.exports = {
+   webpack: {
+      configure: (webpackConfig) => {
+         return {
+            ...webpackConfig,
+            plugins: [
+               ...webpackConfig.plugins,
+               ...whenProd(
+                  () => [
+                     new HtmlCriticalWebpackPlugin({
+                        base: path.resolve(__dirname, 'build'),
+                        src: 'index.html',
+                        dest: 'index.html',
+                        inline: true,
+                        minify: true,
+                        extract: true,
+                        width: 320,
+                        height: 565,
+                        penthouse: {
+                           blockJSRequests: false,
+                        },
+                     }),
+                  ],
+                  []
+               ),
+            ],
+         }
+      },
+   },
+}
