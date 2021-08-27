53d659a6f2d97ccbd56bc67daff858c0af868533
juancathree <juancarloslucenamonje@gmail.com>
2021-08-28 00:16:43 +0100

    feat: :sparkles: add cities component

--- src/components/Cities/index.js
+++ src/components/Cities/index.js
@@ -1,3 +1,26 @@
-export default function Cities() {
-  return <h1>Cities</h1>;
-}
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+import useApp from 'hooks/useApp';
+import { IKImage } from 'imagekitio-react';
+import './styles.css';
+
+export default memo(function Cities() {
+   const { cities } = useApp();
+
+   return (
+      <div className="container">
+         <div className="cities-container">
+            {cities.map((city) => (
+               <Link key={city} className="cities__item" to={city}>
+                  <IKImage
+                     path={city + '.jpg'}
+                     loading="lazy"
+                     className="cities__img"
+                  />
+                  <h2 className="cities__title">{city.toUpperCase()}</h2>
+               </Link>
+            ))}
+         </div>
+      </div>
+   );
+});
