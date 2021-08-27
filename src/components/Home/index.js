117d880e3b05e08405f39f357193462a6573a353
juancathree <juancarloslucenamonje@gmail.com>
2021-08-28 00:16:09 +0100

    feat: :sparkles: add app context

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,26 @@
-import { lazy } from "react";
-import { Switch, Route } from "react-router-dom";
-import Navbar from "../Navbar/index.js";
+import { lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from 'components/Navbar';
+import AppProvider from 'context/AppContext';
+import { IKContext } from 'imagekitio-react';
 
-const Cities = lazy(() => import("../Cities/index.js"));
-const Travels = lazy(() => import("../Travels/index.js"));
-const User = lazy(() => import("../User/index.js"));
+const Cities = lazy(() => import('components/Cities'));
+const Travels = lazy(() => import('components/Travels'));
+const User = lazy(() => import('components/User'));
+const City = lazy(() => import('components/City'));
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Cities} exact />
-        <Route path="/travels" component={Travels} exact />
-        <Route path="/user" component={User} exact />
-      </Switch>
-    </>
-  );
+   return (
+      <AppProvider>
+         <Navbar />
+         <IKContext urlEndpoint="https://ik.imagekit.io/aacivfepey/">
+            <Switch>
+               <Route path="/" component={Cities} exact />
+               <Route path="/travels" component={Travels} exact />
+               <Route path="/user" component={User} exact />
+               <Route path="/:id" component={City} exact />
+            </Switch>
+         </IKContext>
+      </AppProvider>
+   );
 }
