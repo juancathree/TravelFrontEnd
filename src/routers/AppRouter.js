e4dc8f73190535c0d1162a4992263ac375f626c7
juancathree <juancarloslucenamonje@gmail.com>
2021-08-28 00:13:02 +0100

    feat: :sparkles: add route of cities

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,25 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
-const Login = lazy(() => import("../components/Login/index.js"));
-const Signup = lazy(() => import("../components/Signup/index.js"));
-const Home = lazy(() => import("../components/Home/index.js"));
+const Login = lazy(() => import('components/Login'));
+const Signup = lazy(() => import('components/Signup'));
+const Home = lazy(() => import('components/Home'));
 
 export default function AppRouter() {
-  return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute path="/" component={Home} exact />
-          <PrivateRoute path="/travels" component={Home} exact />
-          <PrivateRoute path="/user" component={Home} exact />
-          <Route path="/login" component={Login} exact />
-          <Route path="/signup" component={Signup} exact />
-          <Route path="*" />
-        </Switch>
-      </BrowserRouter>
-    </Suspense>
-  );
+   return (
+      <Suspense fallback={<h1>Loading...</h1>}>
+         <BrowserRouter>
+            <Switch>
+               <PrivateRoute path="/" component={Home} exact />
+               <PrivateRoute path="/travels" component={Home} exact />
+               <PrivateRoute path="/user" component={Home} exact />
+               <Route path="/login" component={Login} exact />
+               <Route path="/signup" component={Signup} exact />
+               <PrivateRoute path="/:id" component={Home} exact />
+               <Route path="*" />
+            </Switch>
+         </BrowserRouter>
+      </Suspense>
+   );
 }
