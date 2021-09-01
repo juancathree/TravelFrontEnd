da42f769ae04192364279f38be10a8111ee53699
juancathree <juancarloslucenamonje@gmail.com>
2021-09-01 23:51:36 +0100

    refactor: :recycle: delete translations

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorkerRegistration from 'serviceWorkerRegistration';
-import i18next from 'i18next';
-import { I18nextProvider } from 'react-i18next';
-import detector from 'i18next-browser-languagedetector';
 import App from './App';
-import 'normalize.css';
-import 'colors.css';
+import 'styles/normalize.css';
+import 'styles/_colors.scss';
+import 'styles/font-family.scss';
 import 'react-notifications/lib/notifications.css';
-import global_es from 'translations/es/global.json';
-import global_en from 'translations/en/global.json';
-
-const options = {
-   order: ['navigator'],
-};
-
-i18next.use(detector).init({
-   detection: options,
-   interpolation: { escapeValue: false },
-   fallbackLng: 'en',
-   resources: {
-      es: {
-         global: global_es,
-      },
-      en: {
-         global: global_en,
-      },
-   },
-});
 
 ReactDOM.render(
    <React.StrictMode>
-      <I18nextProvider i18n={i18next}>
-         <App />
-      </I18nextProvider>
+      <App />
    </React.StrictMode>,
    document.getElementById('root')
 );
