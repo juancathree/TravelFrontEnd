e9558c929b26382d8c33dbadf65a2463aa9a9e9f
juancathree <juancarloslucenamonje@gmail.com>
2021-09-01 23:49:29 +0100

    refactor: :recycle: change all component

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,47 +1,69 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import FormInput from 'components/FormInput';
+import 'components/Login/styles.scss';
 
 export default function Signup() {
-   const [t] = useTranslation('global');
-   const handleSubmit = (e) => {};
+   const [email, setEmail] = useState();
+   const [setPassword] = useState('');
+   const [setName] = useState('');
+
+   const updateEmail = (em) => {
+      setEmail(em);
+   };
+
+   const updatePassword = (pa) => {
+      setPassword(pa);
+   };
+
+   const updateName = (na) => {
+      setName(na);
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      // login({ email, password });
+   };
 
    return (
-      <div className="container">
-         <div className="form">
-            <form onSubmit={handleSubmit} className="signin">
-               <h2 className="title">{t('signup.title')}</h2>
-               <div className="input-field">
-                  <FontAwesomeIcon className="icon" icon={faUser} />
-                  <input
-                     className="input"
-                     type="text"
-                     placeholder={t('signup.name')}
-                  />
-               </div>
-               <div className="input-field">
-                  <FontAwesomeIcon className="icon" icon={faEnvelope} />
-                  <input
-                     className="input"
-                     type="email"
-                     placeholder={t('signup.email')}
-                  />
-               </div>
-               <div className="input-field">
-                  <FontAwesomeIcon className="icon" icon={faLock} />
-                  <input
-                     className="input"
-                     type="password"
-                     placeholder={t('signup.password')}
-                  />
-               </div>
-               <button className="btn">{t('signup.signup')}</button>
+      <div className="login">
+         <div className="login__container">
+            <form className="login__form" onSubmit={handleSubmit}>
+               <h2 className="login__title">
+                  <span className="title__first">City</span>
+                  <span className="title__last">Walker</span>
+               </h2>
+               <FormInput
+                  id="name"
+                  type="text"
+                  placeholder="Nombre"
+                  update={updateName}
+                  value=""
+                  icon={faUser}
+               />
+               <FormInput
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  update={updateEmail}
+                  value={email}
+                  icon={faEnvelope}
+               />
+               <FormInput
+                  id="password"
+                  type="password"
+                  placeholder="Contraseña"
+                  update={updatePassword}
+                  value=""
+                  icon={faLock}
+               />
+               <button className="login__btn">Registrarse</button>
             </form>
             <div className="signup">
                <h4>
-                  {t('signup.registered')}{' '}
-                  <Link to="/login">{t('signup.login')}</Link>
+                  ¿Ya tienes cuenta?
+                  <Link to="/login"> Entrar</Link>
                </h4>
             </div>
          </div>
