addada075a02c726b7f5e1160e4da0005a8b6947
juancathree <juancarloslucenamonje@gmail.com>
2021-09-01 23:53:15 +0100

    feat: :sparkles: create component FormInput

---
+++ src/components/FormInput/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styles.scss';
+
+export default function FormInput({
+   update,
+   id,
+   type,
+   value,
+   placeholder,
+   icon,
+}) {
+   const [state, setState] = useState(value);
+   return (
+      <div className="input">
+         <FontAwesomeIcon className="input__icon" icon={icon} />
+         <input
+            id={id}
+            className="input__field"
+            type={type}
+            placeholder={placeholder}
+            onChange={(e) => {
+               setState(e.target.value);
+               update(e.target.value);
+            }}
+            value={state}
+         />
+      </div>
+   );
+}
