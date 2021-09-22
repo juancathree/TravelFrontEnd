dadf8de961d19987a39686f9a990fb29bc9f0f2d
juancathree <juancarloslucenamonje@gmail.com>
2021-09-23 00:29:20 +0100

    feat: :sparkles: change cookies and add new methods

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,48 +1,52 @@
 import { useContext, useCallback, useState } from 'react';
 import { AuthContext } from 'context/AuthContext';
 import loginService from 'services/login';
-import Cookies from 'universal-cookie';
+import signupService from 'services/signup';
 
 export default function useAuth() {
    const { jwt, setJWT } = useContext(AuthContext);
-   const [state, setState] = useState({ loading: false, error: false });
+   const [state, setState] = useState({
+      loading: false,
+      error: false,
+      isRegister: false,
+   });
 
    const login = useCallback(
       ({ email, password }) => {
          setState({ loading: true, error: false });
-         const cookies = new Cookies();
          loginService({ email, password })
             .then((jwt) => {
                let d = new Date();
                d.setTime(d.getTime() + 120 * 24 * 60 * 60 * 1000);
-               cookies.set('travelapp', jwt, {
-                  path: '/',
-                  sameSite: 'Lax',
-                  secure: true,
-                  expires: d,
-               });
+               document.cookie =
+                  'travelapp=' + jwt + ';expires=' + d + ';path=/';
                setState({ loading: false, error: false });
                setJWT(jwt);
             })
             .catch((err) => {
-               cookies.remove('travelapp');
                setState({ loading: false, error: true });
             });
       },
       [setJWT]
    );
 
-   const logout = useCallback(() => {
-      const cookies = new Cookies();
-      cookies.remove('travelapp');
-      setJWT(null);
-   }, [setJWT]);
+   const signup = useCallback(({ name, email, password }) => {
+      setState({ loading: true, error: false });
+      signupService({ name, email, password })
+         .then((response) => {
+            setState({ loading: false, error: false, isRegister: true });
+         })
+         .catch((err) => {
+            setState({ loading: false, error: true, isRegister: false });
+         });
+   }, []);
 
    return {
       isLogged: Boolean(jwt),
-      isLoginLoading: state.loading,
-      hasLoginError: state.error,
+      isLoading: state.loading,
+      hasError: state.error,
+      isRegister: state.isRegister,
       login,
-      logout,
+      signup,
    };
 }
