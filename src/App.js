25fc5f8e68fa93775959800da1db6367b3fef22c
juancathree <juancarloslucenamonje@gmail.com>
2021-09-23 00:19:59 +0100

    feat: :sparkles: add IKContext and Notification container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import AppRouter from 'routers/AppRouter';
 import AuthProvider from 'context/AuthContext';
+import { NotificationContainer } from 'react-notifications';
+import { IKContext } from 'imagekitio-react';
 
 export default function App() {
    return (
       <AuthProvider>
-         <AppRouter />
+         <IKContext urlEndpoint="https://ik.imagekit.io/aacivfepey/">
+            <NotificationContainer />
+            <AppRouter />
+         </IKContext>
       </AuthProvider>
    );
 }
