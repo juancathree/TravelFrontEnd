7d1e7a0f3820303189d0367d0d1692c1d5925b93
juancathree <juancarloslucenamonje@gmail.com>
2021-10-03 02:30:39 +0100

    create user component

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,3 +1,21 @@
-export default function User() {
-   return <h1>User</h1>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import useAuth from 'hooks/useAuth';
+import './styles.scss';
+
+export default function Travels() {
+   const { logout } = useAuth();
+
+   return (
+      <div className="container">
+         <h3 className="user">Usuario</h3>
+         <FontAwesomeIcon icon={faUserCircle} className="user__icon" />
+         <div className="user__info">
+            <h5>{window.localStorage.getItem('email')}</h5>
+         </div>
+         <button className="btn-logout" onClick={logout}>
+            <p>Salir</p>
+         </button>
+      </div>
+   );
 }
